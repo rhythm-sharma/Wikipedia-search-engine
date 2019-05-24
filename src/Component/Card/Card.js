e4d59c94f96d602cd911ebbb7101e6572508c9ad
Rhythm <rrhythmsharma@gmail.com>
2019-05-24 14:45:10 +0530

    Random article: add random article link

--- src/Component/Card/Card.js
+++ src/Component/Card/Card.js
@@ -7,10 +7,10 @@ AOS.init();
 const Card = (props) => {
     const directLink = `https://en.wikipedia.org/?curid=${props.pageid}`;
     return(
-      <div className="center grow maxwidth padding">
+      <div className="center grow card">
           <article className="grow center p2 wrapper hidden  b--black-10 mv4" data-aos="fade-up">
           <h1 className="f4 bg-near-white black-60 mv0 pv2 ph3">
-              <a className="black no-underline underline-hover" href={directLink} target="blank">{props.title}</a>
+              <a className="title black no-underline underline-hover" href={directLink} target="blank">{props.title}</a>
           </h1>
           <div className="pa3 bt b--black-10 bg-white">
           <p dangerouslySetInnerHTML={{__html: props.snippet}} className="f6 f5-ns lh-copy mv0"></p>
