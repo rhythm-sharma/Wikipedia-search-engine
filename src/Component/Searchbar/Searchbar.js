6a5fe435856ee6d4ff85088849933a2fb485b922
Rhythm <rrhythmsharma@gmail.com>
2019-05-24 14:45:10 +0530

    Random article: add random article link

--- src/Component/Searchbar/Searchbar.js
+++ src/Component/Searchbar/Searchbar.js
@@ -4,9 +4,9 @@ import './Searchbar.css'
 const Searchbar = ({fetchResults ,searchChange}) => {
         return(
             <div className='container'>
-                <div className="heading">
-                    <h1>W</h1><h2>IKIPEDI</h2><h1>A</h1>
-                </div>
+                <h1 class="f2 f1-l fw2 white-90 mb0 lh-title">Wikipedia</h1>
+                <h2 class="fw1 f3 white-80 mt3 mb4">Search Engine</h2>
+                <p class="white-80 mt3 mb4"><a className="white randon-link" href="https://en.wikipedia.org/wiki/Special:Random"  target="blank"><i class="fas fa-link"></i>Click here for a random article</a></p>
                 <div className="search-form ba b--white">
                     <input onChange={searchChange} type="text" className="search-field" placeholder="What are you looking for?" />
                     <button onClick={fetchResults}  type="submit" className="button-search"><i className="fa fa-search fa-lg"></i></button>
